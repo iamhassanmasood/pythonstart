95cc93c16b70d843e19f771277abfd494e20ae20
Hassan Masood <iamhassanmasood@gmail.com>
2019-05-16 05:30:55 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,45 @@
+# print('Hello World')
+# x = 'johnny '
+# y = 'sins '
+# z = 'is bastard'
+# print(x+y+z)
+# if 5>2: 
+#     print('Five is greater than 2')
+# a = 5
+# b = 2.67
+# print(a + b)
+# print(type(a))
+# print(type(b))
+# print(type(x))
+
+# h = int("4")
+# k = int("6")
+# print(h+k)
+
+# chk = "Hello World"
+# print(chk[0])
+# print(chk[1])
+# print(chk[1:5])
+story = "  A quick browm fox jumps over the lazy dog"
+print(story.split(' '))
+print(story.split(','))
+
+# print(story)
+# print(len(story))
+# print(story.strip())
+# print(story.lower())
+# print(story.upper())
+# print(story.replace("A" , "B"))
+
+
+cost = 1 + 33 +3333*5
+print(cost)
+
+x = input("Please enter your name : ")
+print(' Hi '+ x)
+
+# sip = input("Which animal you have?:  ")
+if sip == "cat"
+    status = "Ok"
+    
+    print("You have brown cat am i right?")
