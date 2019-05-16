1755d7e0a0fcdd7e099bb27da9277ff4c0c15bf6
Hassan Masood <iamhassanmasood@gmail.com>
2019-05-16 15:20:48 +0500

    first commit

--- test.py
+++ test.py
@@ -1,35 +1,35 @@
-# print('Hello World')
-# x = 'johnny '
-# y = 'sins '
-# z = 'is bastard'
-# print(x+y+z)
-# if 5>2: 
-#     print('Five is greater than 2')
-# a = 5
-# b = 2.67
-# print(a + b)
-# print(type(a))
-# print(type(b))
-# print(type(x))
+print('Hello World')
+x = 'johny '
+y = 'tony '
+z = 'is bastard'
+print(x+y+z)
+if 5>2: 
+    print('Five is greater than 2')
+a = 5
+b = 2.67
+print(a + b)
+print(type(a))
+print(type(b))
+print(type(x))
 
-# h = int("4")
-# k = int("6")
-# print(h+k)
+h = int("4")
+k = int("6")
+print(h+k)
 
-# chk = "Hello World"
-# print(chk[0])
-# print(chk[1])
-# print(chk[1:5])
+chk = "Hello World"
+print(chk[0])
+print(chk[1])
+print(chk[1:5])
 story = "  A quick browm fox jumps over the lazy dog"
 print(story.split(' '))
 print(story.split(','))
 
-# print(story)
-# print(len(story))
-# print(story.strip())
-# print(story.lower())
-# print(story.upper())
-# print(story.replace("A" , "B"))
+print(story)
+print(len(story))
+print(story.strip())
+print(story.lower())
+print(story.upper())
+print(story.replace("A" , "B"))
 
 
 cost = 1 + 33 +3333*5
@@ -38,8 +38,6 @@ print(cost)
 x = input("Please enter your name : ")
 print(' Hi '+ x)
 
-# sip = input("Which animal you have?:  ")
-if sip == "cat"
-    status = "Ok"
-    
-    print("You have brown cat am i right?")
+sip = input("Which animal you have?:  ")
+
+print("You have " + sip)
