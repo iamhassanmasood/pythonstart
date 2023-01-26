3ab285d340077f39edda395568fe50cf3c05de97
Hassan Masood <iamhassanmasood@gmail.com>
2023-01-27 01:59:23 +0500

    python learn string slices

--- python-string/index.py
+++ python-string/index.py
@@ -22,3 +22,21 @@ consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
 print(multilineText) #Multiline text
+
+b = "Hello, World!"
+print(b[2:5]) #it will print from position 2 to position 5 (not included)
+
+b = "Hello, World!"
+print(b[:5]) #it witll print from the start to position 5 (not included)
+
+b = "Hello, World!"
+print(b[2:]) #it will print from position 2, and all the way to the end
+
+b = "Hello, Engineers"
+print(b[-5:-2]) #it will print in negative order from position 2 to position 5 (not included)
+
+b = "Hello, Engineers"
+print(b[:-2]) #it will print in negative order from position 2 to position end
+
+b = "Hello, Engineers"
+print(b[-2:]) #it will print last 2 index's
