d122d9081ede71686e04db792a99a8aee21473a0
Hassan Masood <iamhassanmasood@gmail.com>
2023-01-26 22:05:20 +0500

    python strings

--- python-string/index.py
+++ python-string/index.py
@@ -8,3 +8,11 @@ x = "Hi Developer"
 print(x)
 print(x[1]) #it will be 'i'
 print(len(x)) #it will print length of string x
+
+txt = "Nothing in life comes for free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+newText = "A quick brown fox jumps over the lazy dog"
+print("free" in newText) #False
+print("expensive" not in txt) #True
