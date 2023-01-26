d2d48da26cb6966942c35ec0f9341095f33578e2
Hassan Masood <iamhassanmasood@gmail.com>
2023-01-26 21:56:42 +0500

    python string and methods

---
+++ python-string/index.py
@@ -0,0 +1,10 @@
+a = "Hello"
+print(a) 
+print(type(a))   #it will be 'str'
+
+for i in a:
+    print(i)
+x = "Hi Developer"
+print(x)
+print(x[1]) #it will be 'i'
+print(len(x)) #it will print length of string x
