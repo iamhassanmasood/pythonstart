27eec9ccff6232176f09d680376f4158bfd8ec5b
Hassan Masood <iamhassanmasood@gmail.com>
2023-02-11 23:08:24 +0500

    operators in python

---
+++ if-else-python/operators.py
@@ -0,0 +1,14 @@
+a = 5
+b = 4
+
+if(a>b and b==0):
+    print("Hello")
+
+message = ""
+SAT = 66
+avg = 78
+GPA = 3.31
+parent = "ex"
+if SAT > avg or GPA > 2.5 or parent == "alum":
+    message = "Welcome to Leeds College!"
+print(message)
