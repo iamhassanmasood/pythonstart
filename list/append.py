2baaf6069e618d16b998911b2a61e2f0c7d94856
Hassan Masood <iamhassanmasood@gmail.com>
2023-02-15 22:12:33 +0500

    python: append multiples in list

--- list/append.py
+++ list/append.py
@@ -2,5 +2,8 @@ cities = ["Atlanta", "Baltimore", "Chicago",
 "Denver", "Los Angeles", "Seattle"]
 
 cities.append("Karachi")
+print(cities)
+
+cities = cities + ["Dubuque", "New Orleans"]
 
 print(cities)
